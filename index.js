0520a2d31d40a6ae2ec202aa26c1d0b45088e745
Yackalips <111602724+Yackalips@users.noreply.github.com>
2025-02-05 19:36:37 +0000

    test3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("e");
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.resolve()));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
